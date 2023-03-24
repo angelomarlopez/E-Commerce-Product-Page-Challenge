1d2a5409fc45053abf9b05899e66975ad36a2ca5
Angel Lopez <angel@aolopez.com>
2023-03-23 20:54:43 -0500

    Cart Addition & Deletion functionality

--- js/index.js
+++ js/index.js
@@ -25,3 +25,79 @@ closeBg.addEventListener('click', () => {
 cartIcon.addEventListener('click', () => {
     cartContainer.classList.toggle('close');
 });
+
+// Update Current Image and Thumbnail
+let currentThumbnail = document.querySelector('.current-thumbnail');
+let currentImage = document.querySelector('#current-image');
+const thumbnails = document.querySelectorAll('#thumbnail');
+
+thumbnails.forEach( thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        currentThumbnail.classList.toggle('current-thumbnail');
+        currentThumbnail = thumbnail;
+        currentImage.src = thumbnail.childNodes[0].src.substring(thumbnail.childNodes[0].src.indexOf("images/"));
+        thumbnail.classList.toggle('current-thumbnail');
+    })
+});
+
+
+let cartItemCount = 0;
+
+let minusIcon = document.querySelector('#minus');
+let plusIcon = document.querySelector('#plus');
+let addToCartButton = document.querySelector('#add');
+let amountElement = document.querySelector('#amount');
+let price = document.querySelector('#price');
+
+const createCartItem = (price, count) => {
+    let cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    
+    let productThumbnail = document.createElement('img');
+    productThumbnail.src = 'images/image-product-1-thumbnail.jpg';
+    productThumbnail.alt = 'Item Thumbnail';
+
+    let productCartInfo = document.createElement('div');
+
+    let productName = document.createElement('p');
+    productName.innerHTML = 'Fall Limited Edition Sneakers';
+
+    let pricingInfo = document.createElement('p');
+    pricingInfo.innerHTML = `$${price} x ${count} <span id="total">$${parseFloat(price)*parseFloat(count)}.00</span>`
+
+    
+    let deleteIcon = document.createElement('img');
+    deleteIcon.src = 'images/icon-delete.svg';
+    deleteIcon.alt = "Delete Icon";
+    deleteIcon.classList.add('icon');
+    deleteIcon.id = 'delete';
+
+    deleteIcon.addEventListener('click', () => {
+        document.querySelector('.cart-items').removeChild(cartItem);
+    });
+
+    productCartInfo.appendChild(productName);
+    productCartInfo.appendChild(pricingInfo);
+
+    cartItem.appendChild(productThumbnail);
+    cartItem.appendChild(productCartInfo);
+    cartItem.appendChild(deleteIcon);
+
+    return cartItem;
+}
+
+minusIcon.addEventListener('click', () => {
+    amountElement.innerHTML = amountElement.innerHTML == 0 ? 0 : amountElement.innerHTML-1;
+});
+
+plusIcon.addEventListener('click', () => {
+    amountElement.innerHTML = parseInt(amountElement.innerHTML)+1
+});
+
+addToCartButton.addEventListener('click', () => {
+    if (amountElement.innerHTML != 0) {
+        document.querySelector('.cart-items').appendChild(createCartItem(price.innerHTML, amountElement.innerHTML));
+
+        amountElement.innerHTML = 0;
+    }
+});
